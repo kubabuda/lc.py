cf6219201d54addc3efd92cda332a10227b34849
Kuba Buda <kubabuda@gazeta.pl>
2022-09-07 07:14:08 +0200

    33. Search in Rotated Sorted Array

---
+++ src/0033_search_in_rotated_sorted_array.py
@@ -0,0 +1,55 @@
+from typing import *
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # find pivot point
+        begin = 0
+        end = len(nums) - 1
+        while begin < end:
+            mid = int((begin + end) / 2)
+            if end - begin >= 2 and nums[mid] < nums[mid + 1] < nums[mid - 1]:
+                begin = mid
+                break
+            elif nums[mid] > nums[end]: # pivot in right half
+                begin = mid + 1
+            else:   # pivot point in left half
+                end = mid - 1
+        pivot_k = begin
+        # find value
+        begin = 0
+        end = len(nums) - 1
+        while begin <= end:
+            mid = int((begin + end) / 2)
+            mid_rotated = (mid + pivot_k) % len(nums)
+            if nums[mid_rotated] == target:
+                return mid_rotated
+            elif nums[mid_rotated] > target:
+                end = mid - 1
+            else:
+                begin = mid + 1
+        return -1
+
+
+from unittest import TestCase
+import unittest
+
+class Test(unittest.TestCase): 
+    
+    def testCases(self):
+        param_list = [
+            ([4,5,6,7,0,1,2], 0, 4),
+            ([4,5,6,7,0,1,2], 3, -1),
+            ([1], 0, -1),
+            ([5,1,3], 5, 0)
+        ]
+        for nums, target, expected in param_list:
+            with self.subTest():
+                # arrange
+                s = Solution()
+                # act
+                result = s.search(nums, target)
+                # assert
+                self.assertEqual(expected, result, (nums, target))
+
+if __name__ == '__main__':
+    unittest.main()
