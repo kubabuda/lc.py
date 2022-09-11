9c989ef48a261ac700073c58270c14bceafa1c8d
Kuba Buda <kubabuda@gazeta.pl>
2022-09-11 14:42:29 +0200

    207. Course Schedule: deque stack instance reused

--- src/0207_course_schedule.py
+++ src/0207_course_schedule.py
@@ -1,13 +1,19 @@
 from typing import *
+from collections import deque
 # https://leetcode.com/problems/course-schedule/
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        nodes = { i: [] for i in range(0, numCourses) } # course: [precedingCourse]
+        # populate hashSet { course: [precedingCourse] }
+        nodes = { i: [] for i in range(0, numCourses) }
         for p in prerequisites:
             nodes[p[0]].append(p[1])
+        # iterate over every course
+        visited = set()
+        to_visit = deque()
         for course in range(0, numCourses):
-            visited = set()
-            to_visit = [course]
+            to_visit.clear()
+            to_visit.append(course)
+            visited.clear()
             while to_visit:
                 curr = to_visit.pop()
                 visited.add(curr)
@@ -23,10 +29,11 @@ import unittest
 class TestTemplate(unittest.TestCase): 
     def testCases(self):
         param_list = [
-            # (2, [], True),
-            # (2, [[1,0]], True),
+            (2, [], True),
+            (2, [[1,0]], True),
             (2, [[1,0],[0,1]], False),
-            # (5, [[1,4],[2,4],[3,1],[3,2]], True)
+            (5, [[1,4],[2,4],[3,1],[3,2]], True),
+            (4, [[0,1],[3,1],[1,3],[3,2]], False),
         ]
         for numCourses, nodes, expected in param_list:
             with self.subTest():
