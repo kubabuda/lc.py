73e2ac96b4a02d1570cf0d0e09dc68980ef01014
Kuba Buda <kubabuda@gazeta.pl>
2022-09-14 14:35:16 +0200

    200. Number of Islands: iterative BFS and DFS

--- src/0200_number_of_islands.py
+++ src/0200_number_of_islands.py
@@ -1,8 +1,10 @@
 from typing import *
+import collections
 # https://leetcode.com/problems/number-of-islands
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        '''recursive DFS'''
         visited = set()
         islands = 0
         max_r = len(grid)
@@ -24,43 +26,129 @@ class Solution:
         
         return islands
 
+    def numIslands2(self, grid: List[List[str]]) -> int:
+        '''iterative BFS'''
+        visited = set()
+        islands = 0
+        max_r = len(grid)
+        max_c = len(grid[0])
+        
+        def bfs(rc):
+            to_visit = collections.deque()
+            to_visit.append(rc)
+
+            while(to_visit):
+                rc = to_visit.popleft()
+                r,c = rc
+                if not rc in visited and grid[r][c] != '0':
+                    if r > 0: to_visit.append((r - 1, c))
+                    if c > 0: to_visit.append((r, c - 1))
+                    if r + 1 < max_r: to_visit.append((r + 1, c))
+                    if c + 1<  max_c: to_visit.append((r, c + 1))
+                    visited.add(rc)
+        
+        for r in range(max_r):
+            for c in range(max_c):
+                if grid[r][c] != '0':
+                    rc = (r,c) 
+                    if rc not in visited:
+                        bfs(rc)
+                        islands += 1
+        return islands
+
+    def numIslands3(self, grid: List[List[str]]) -> int:
+        '''iterative DFS'''
+        visited = set()
+        islands = 0
+        max_r = len(grid)
+        max_c = len(grid[0])
+        
+        def bfs(rc):
+            to_visit = collections.deque()
+            to_visit.append(rc)
+
+            while(to_visit):
+                rc = to_visit.pop()
+                r,c = rc
+                if not rc in visited and grid[r][c] != '0':
+                    if r > 0: to_visit.append((r - 1, c))
+                    if c > 0: to_visit.append((r, c - 1))
+                    if r + 1 < max_r: to_visit.append((r + 1, c))
+                    if c + 1<  max_c: to_visit.append((r, c + 1))
+                    visited.add(rc)
+        
+        for r in range(max_r):
+            for c in range(max_c):
+                if grid[r][c] != '0':
+                    rc = (r,c) 
+                    if rc not in visited:
+                        bfs(rc)
+                        islands += 1
+        return islands
+
 from unittest import TestCase
 import unittest
 class TestTemplate(unittest.TestCase): 
+    param_list = param_list = [
+        ([
+            ["0","0","0","0","0"],
+            ["0","0","0","0","0"],
+            ["0","0","0","0","0"],
+            ["0","0","0","0","0"]
+        ], 0),
+        ([
+            ["1","1","1","1","1"],
+            ["1","1","1","1","1"],
+            ["1","1","1","1","1"],
+            ["1","1","1","1","1"]
+        ], 1),
+        ([
+            ["1","1","1","1","0"],
+            ["1","1","0","0","0"],
+            ["0","0","0","0","0"],
+            ["0","0","0","1","1"]
+        ], 2),
+        ([
+            ["1","1","1","1","0"],
+            ["1","1","0","1","0"],
+            ["1","1","0","0","0"],
+            ["0","0","0","0","0"]
+        ], 1),
+        ([
+            ["1","1","0","0","0"],
+            ["1","1","0","0","0"],
+            ["0","0","1","0","0"],
+            ["0","0","0","1","1"]
+        ], 3),
+    ]
 
     def testCases(self):
-        param_list = [
-            ([
-                ["0","0","0","0","0"],
-                ["0","0","0","0","0"],
-                ["0","0","0","0","0"],
-                ["0","0","0","0","0"]
-            ], 0),
-            ([
-                ["1","1","1","1","1"],
-                ["1","1","1","1","1"],
-                ["1","1","1","1","1"],
-                ["1","1","1","1","1"]
-            ], 1),
-            ([
-                ["1","1","1","1","0"],
-                ["1","1","0","1","0"],
-                ["1","1","0","0","0"],
-                ["0","0","0","0","0"]
-            ], 1),
-            ([
-                ["1","1","0","0","0"],
-                ["1","1","0","0","0"],
-                ["0","0","1","0","0"],
-                ["0","0","0","1","1"]
-            ], 3),
-        ]
-        for grid, expected in param_list:
+        for grid, expected in self.param_list:
             with self.subTest():
                 # arrange
                 s = Solution()
                 # act
                 result = s.numIslands(grid)
+                # assert
+                self.assertEqual(expected, result, (grid))
+    
+    def testCases2(self):
+        for grid, expected in self.param_list:
+            with self.subTest():
+                # arrange
+                s = Solution()
+                # act
+                result = s.numIslands2(grid)
+                # assert
+                self.assertEqual(expected, result, (grid))
+
+    def testCases3(self):
+        for grid, expected in self.param_list:
+            with self.subTest():
+                # arrange
+                s = Solution()
+                # act
+                result = s.numIslands3(grid)
                 # assert
                 self.assertEqual(expected, result, (grid))
 
