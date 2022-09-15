67c664cea183d24d3d6fbe60013634ea85d86e53
Kuba Buda <kubabuda@gazeta.pl>
2022-09-15 10:27:30 +0200

    269. Alien Dictionary: added input validation logic

--- src/0269_alien_dictionary.py
+++ src/0269_alien_dictionary.py
@@ -9,12 +9,15 @@ class Solution:
         # build prerequisites dictionary
         preceeding = { c: set() for c in letters }
 
-        def bfs_populate_preceeding(words, prefix):
+        def bfs_populate_preceeding(words, prefix)->bool:
             if not words: return
             prefix_len = len(prefix)
             c_words = {}
+            prev_word = ''
 
             for c_word in words:
+                if len(c_word) < len(prev_word) and c_word in prev_word:
+                    return False    # invalid input data
                 if len(c_word) > prefix_len:
                     c = c_word[prefix_len]
                     if c not in c_words:
@@ -24,13 +27,16 @@ class Solution:
                                 preceeding[c].add(i)
                     if len(c_word) > prefix_len:
                         c_words[c].append(c_word)
+                prev_word = c_word
+
             for c in c_words:
                 ch_words = c_words[c]
                 if words:
                     c_prefix = f'{prefix}{c}'
-                    bfs_populate_preceeding(ch_words, c_prefix)
+                    if not bfs_populate_preceeding(ch_words, c_prefix): return False
+            return True
             
-        bfs_populate_preceeding(words, '')
+        if not bfs_populate_preceeding(words, ''): return ''
         
         result = []
         resultSet = set()
@@ -55,6 +61,7 @@ class TestTemplate(unittest.TestCase):
     
     param_list = [
         (['wrt','wrf','er','ett','rftt'], 'wertf'),
+        (['wrt','wrf','er','ett','rftt','rft'], ''),
     ]
 
     def testCases(self):
