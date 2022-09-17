f0fd9656d7df6c2d507c1abd258e9b820d2bd664
Kuba Buda <kubabuda@gazeta.pl>
2022-09-17 21:53:52 +0200

    323. Number of Connected Components in an Undirected Graph: correct union find approach

--- src/0323_number_of_connected_in_an_undirected_graph.py
+++ src/0323_number_of_connected_in_an_undirected_graph.py
@@ -25,20 +25,31 @@ class Solution:
     def countComponents_unionFind(self, n: int, edges: List[List[int]]) -> int:
         if n == 0 or not edges: return n
         parent = [i for i in range(n)]
+        rank = [i for i in range(n)]
 
-        for i,j in edges:
-            i_root = parent[i]
-            while i_root != parent[i_root]:
-                i_root = parent[i_root]
-            parent[i] = i_root
-            parent[j] = i_root
+        def find(n):
+            p = n
+            while p != parent[p]:
+                parent[p] = parent[parent[p]]
+                p = parent[p]
+            return p
 
-        result = set()
-        for i in parent:
-            if i not in result:
-                result.add(i)
-
-        return len(result)
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2: return 0
+            
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            return 1
+        
+        result = n
+        for n1, n2 in edges:
+            result -= union(n1, n2)
+        return result
 
 from unittest import TestCase
 import unittest
@@ -52,7 +63,7 @@ class TestTemplate(unittest.TestCase):
         (5, [[0, 1], [1, 2], [3, 4]], 2),
         (6, [[0,1],[0,2],[0,3],[1,4]], 2),
         (4, [[3, 2], [2, 1], [3, 0]], 1),
-        # (15, [[13, 8], [10, 5], [11, 10], [8, 7], [10, 4], [5, 3], [7, 0], [11, 9], [14, 9], [13, 6], [8, 6], [1, 0], [6, 3], [9, 4]], 3),
+        (15, [[13, 8], [10, 5], [11, 10], [8, 7], [10, 4], [5, 3], [7, 0], [11, 9], [14, 9], [13, 6], [8, 6], [1, 0], [6, 3], [9, 4]], 3),
         (15, [(0, 1), (0, 7), (4, 10), (10, 11), (4, 9), (6, 8), (8, 13), (5, 10), (6, 13), (7, 8), (3, 6), (9, 14), (9, 11), (3, 5)], 3)
     ]
 
