e5ad863a55192e27ba9a4d351640e138310b3941
Kuba Buda <kubabuda@gazeta.pl>
2022-09-23 10:19:45 +0200

    253. Meeting Rooms II: sliding window/two pointer approach

--- src/0253_meeting_rooms_II.py
+++ src/0253_meeting_rooms_II.py
@@ -5,6 +5,21 @@ from typing import *
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        # O(n log n) time
+        intervals.sort()
+        result = 0
+        start = 0
+        end = 0
+        while start < len(intervals):
+            if start >= end or (end + 1 < len(intervals) and intervals[end + 1][0] < intervals[start][1]):
+                end += 1
+            else:
+                start += 1
+            result = max(result, end - start)
+        return result
+        
+    def minMeetingRooms2(self, intervals: List[List[int]]) -> int:
+        # O(n**2) time pessimistic
         intervals.sort()
         result = 0
         overlaps = []
@@ -34,6 +49,16 @@ class SolutionTests(unittest.TestCase):
                 s = Solution()
                 # act
                 result = s.minMeetingRooms(nums)
+                # assert
+                self.assertEqual(expected, result, (nums))
+
+    def testCases_minMeetingRooms2(self):
+        for nums, expected in self.param_list:
+            with self.subTest():
+                # arrange
+                s = Solution()
+                # act
+                result = s.minMeetingRooms2(nums)
                 # assert
                 self.assertEqual(expected, result, (nums))
 
