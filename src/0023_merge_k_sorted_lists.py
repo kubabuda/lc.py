a480fb58b25e301b2e35144dd0d89e9388cce80f
Kuba Buda <kubabuda@gazeta.pl>
2022-09-24 18:56:22 +0200

    23. Merge k Sorted Lists: minheap approach

--- src/0023_merge_k_sorted_lists.py
+++ src/0023_merge_k_sorted_lists.py
@@ -1,26 +1,27 @@
 from typing import *
 # 23. Merge k Sorted Lists
 # https://leetcode.com/problems/merge-k-sorted-lists
+import heapq
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next    
 
 class Solution:
-    def mergeTwoLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        """one big min heap approach"""
         result = None
         r_tail = None
-        tails = [l for l in lists if l]
-        while tails:
-            i_min = 0
-            for i, t in enumerate(tails):
-                if t.val < tails[i_min].val:
-                    i_min = i
-            to_add = tails[i_min]
-            if not to_add.next:
-                tails.remove(to_add)
-            else:
-                tails[i_min] = to_add.next
+        
+        minheap = []
+        for node in lists:
+            while node:
+                heapq.heappush(minheap, node.val)
+                node = node.next
+                
+        while minheap:
+            val = heapq.heappop(minheap)
+            to_add = ListNode(val=val)
             if not result:
                 result = to_add
                 r_tail = result
@@ -28,7 +29,6 @@ class Solution:
                 r_tail.next = to_add
                 r_tail = r_tail.next
         return result
-
 
 from unittest import TestCase
 import unittest
@@ -41,14 +41,14 @@ class SolutionTests(unittest.TestCase):
         ([[], [0]], [0]),
     ]
 
-    def testCases_mergeTwoLists(self):
+    def testCases_mergeKLists(self):
         for nums, expected in self.param_list:
             with self.subTest():
                 # arrange
                 s = Solution()
                 lists = [ LoadListNode(n) for n in nums ]
                 # act
-                result = s.mergeTwoLists(lists)
+                result = s.mergeKLists(lists)
                 # assert
                 self.assertEqual(expected, ListNodeToArray(result), (nums))
 
