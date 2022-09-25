aa4eaeeca8cf3676abc26b224647c7f745196313
Kuba Buda <kubabuda@gazeta.pl>
2022-09-25 21:06:07 +0200

    143. Reorder List: O(1) space O(n) time solution

--- src/0143_reorder_list.py
+++ src/0143_reorder_list.py
@@ -12,6 +12,7 @@ class ListNode:
 import collections
 class Solution:
     def reorderList(self, head: Optional[ListNode]):
+        """O(n) space O(n) time"""
         nodes = collections.deque()
         tail = head.next
         while tail:
@@ -27,8 +28,46 @@ class Solution:
             else: 
                 curr = nodes.pop()
             tail.next = curr
-            tail = curr
+            tail = tail.next
             i += 1
+
+    def reorderList2(self, head: Optional[ListNode]):
+        """O(1) space O(n) time"""
+        tail = head
+        n = 0
+        while tail:
+            tail = tail.next
+            n += 1          # count nodes in input
+    
+        def reverseLL(node, prev=None):
+            if not node: return prev
+            ne = node.next
+            node.next = prev
+            return reverseLL(ne, node)
+    
+        skip = int(n / 2)
+        if n % 2: skip += 1
+        rhead = None        # after half of input
+        i = 1               # reverse second half
+        tail = head
+        while i < skip:
+            tail = tail.next
+            i += 1
+        
+        rhead = reverseLL(tail.next)
+        tail.next = None    # cutoff reversed half from original
+        result = head       # start from original head
+        head = head.next
+        tail = result
+        while head or rhead:
+            if rhead:       # add one from end (reversed head)
+                tail.next = rhead
+                rhead = rhead.next
+                tail = tail.next
+            if head:        # add one from original head
+                tail.next = head
+                head = head.next
+                tail = tail.next
 
 
 from unittest import TestCase
@@ -49,6 +88,17 @@ class SolutionTests(unittest.TestCase):
                 head = LoadListNode(nums)
                 # act
                 s.reorderList(head)
+                # assert
+                self.assertEqual(expected, ListNodeToArray(head), (nums))
+
+    def testCases_reorderList2(self):
+        for nums, expected in self.param_list:
+            with self.subTest():
+                # arrange
+                s = Solution()
+                head = LoadListNode(nums)
+                # act
+                s.reorderList2(head)
                 # assert
                 self.assertEqual(expected, ListNodeToArray(head), (nums))
 
