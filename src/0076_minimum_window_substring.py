91545359ede542223dcc80a6af8c3eb476f36bef
Kuba Buda <kubabuda@gazeta.pl>
2022-10-05 09:01:32 +0200

    76. Minimum Window Substring: optimized time, more memory eaten

--- src/0076_minimum_window_substring.py
+++ src/0076_minimum_window_substring.py
@@ -21,12 +21,42 @@ class Solution:
 
         for r in range(len(s)):
             count_s[s[r]] += 1
-            while isWindow():
                 
+            while isWindow():
                 if (r+1-l) < minL[1]-minL[0]:
                     minL = (l, r+1)
                 count_s[s[l]] -= 1
                 l += 1
+
+        return s[minL[0]:minL[1]]
+
+
+    def minWindowF(self, s: str, t: str) -> str:
+        count_t = Counter(t)
+        filtr_s = [(i, c) for i,c in enumerate(s) if c in count_t]
+        count_s = Counter(c for i,c in filtr_s)
+
+        def isWindow():
+            for ct in count_t:
+                if ct not in count_s or count_s[ct] < count_t[ct]:
+                    return False
+            return True
+        
+        if not isWindow(): return ""
+
+        count_s = { c: 0 for c in count_t }
+        minL = (0, len(s))
+        filtr_l = 0
+
+        for r, c in filtr_s:
+            count_s[c] += 1
+
+            while isWindow():
+                l, cl = filtr_s[filtr_l]
+                if (r+1-l) < minL[1]-minL[0]:
+                    minL = (l, r+1)
+                count_s[cl] -= 1
+                filtr_l += 1
 
         return s[minL[0]:minL[1]]
 
@@ -50,6 +80,16 @@ class SolutionTests(unittest.TestCase):
                 sol = Solution()
                 # act
                 result = sol.minWindow(s, t)
+                # assert
+                self.assertEqual(expected, result, (s, t))
+
+    def testCases_minWindowF(self):
+        for s, t, expected in self.param_list():
+            with self.subTest():
+                # arrange
+                sol = Solution()
+                # act
+                result = sol.minWindowF(s, t)
                 # assert
                 self.assertEqual(expected, result, (s, t))
 
