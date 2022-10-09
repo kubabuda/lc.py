1c9a095d3cc8033b4451625187effbc99a48e23f
Kuba Buda <kubabuda@gazeta.pl>
2022-10-09 08:17:36 +0200

    49. Group Anagrams: O(m*n) time

--- src/0049_group_anagrams.py
+++ src/0049_group_anagrams.py
@@ -4,6 +4,7 @@ from typing import *
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """O(m*n log n) time"""
         groups = {}
         for s in strs:
             ss = "".join(sorted(s))
@@ -13,6 +14,21 @@ class Solution:
 
         return groups.values()
 
+    def groupAnagramsH(self, strs: List[str]) -> List[List[str]]:
+        """O(m*n) time"""
+        groups = {}
+        ord_a = ord('a')
+        for s in strs:
+            sc = Counter(s)
+            ss = 0
+            for c in (chr(ord_a + i) for i in range(26)):
+                ss |= sc[c]
+                ss <<= 14
+            if ss not in groups:
+                groups[ss] = []
+            groups[ss].append(s)
+
+        return groups.values()
 
 from unittest import TestCase
 import unittest
@@ -25,6 +41,7 @@ class SolutionTests(unittest.TestCase):
         (["a"], [["a"]]),
         (["eat","tea","tan","ate","nat","bat"], [["bat"],["nat","tan"],["ate","eat","tea"]]),
         (["cab","tin","pew","duh","may","ill","buy","bar","max","doc"], [["max"],["buy"],["doc"],["may"],["ill"],["duh"],["tin"],["bar"],["pew"],["cab"]]),
+        (["cat","rye","aye","cud","cat","old","fop","bra"], [["bra"],["old"],["cud"],["aye"],["rye"],["fop"],["cat","cat"]]),
     ]
 
     def testCases_groupAnagrams(self):
@@ -34,6 +51,20 @@ class SolutionTests(unittest.TestCase):
                 sol = Solution()
                 # act
                 result = sol.groupAnagrams(strs)
+                # assert
+                self.maxDiff = None
+                self.assertEqual(
+                    [set(gr) for gr in sorted(sorted(s) for s in expected)], 
+                    [set(gr) for gr in sorted(sorted(s) for s in result)],
+                    (strs))
+
+    def testCases_groupAnagramsH(self):
+        for strs, expected in self.param_list():
+            with self.subTest():
+                # arrange
+                sol = Solution()
+                # act
+                result = sol.groupAnagramsH(strs)
                 # assert
                 self.maxDiff = None
                 self.assertEqual(
