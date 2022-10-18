d569055743bd8d9da20fe783a35ebe8d395d38b9
Kuba Buda <kubabuda@gazeta.pl>
2022-10-18 23:25:44 +0200

    124. Binary Tree Maximum Path Sum

---
+++ src/0124_binary_tree_maximum_path_sum.py
@@ -0,0 +1,86 @@
+from typing import *
+import collections
+import heapq
+# 124. Binary Tree Maximum Path Sum
+# https://leetcode.com/problems/binary-tree-maximum-path-sum/
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    def __repr__(self):
+        return f"TreeNode {self.val}{f' l:({self.left})' if self.left else ''}{f' r:({self.right})' if self.right else ''}"
+
+class Solution:
+
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        minheap = []
+        
+        def maxPathSum(root) -> int:
+            possible = [-root.val] # flip sign to use minheap
+
+            if root.left:
+                l = maxPathSum(root.left)
+                possible.append(l - root.val)
+            if root.right: 
+                r = maxPathSum(root.right)
+                possible.append(r - root.val)
+            if root.left and root.right:
+                heapq.heappush(minheap, (r + l - root.val))
+
+            for p in possible: heapq.heappush(minheap, p)
+            
+            return min(possible)
+    
+        maxPathSum(root)
+
+        return 0 - minheap[0] # flip sign back
+
+
+from unittest import TestCase
+import unittest
+import collections
+
+null = None
+class SolutionTests(unittest.TestCase):
+    
+    param_list = [
+        # ([1,2,3], 6),
+        # ([-10,9,20,null,null,15,7], 42),
+        # ([1,-2,-3,1,3,-2,null,-1], 3),
+        ([5,4,8,11,null,13,4,7,2,null,null,null,1], 48),
+    ]
+
+    def testCases_maxPathSum(self):
+        for nums, expected in self.param_list:
+            with self.subTest():
+                # arrange
+                s = Solution()
+                root = buildTree(nums)
+                # act
+                result = s.maxPathSum(root)
+                # assert
+                self.assertEqual(expected, result, (nums, root))
+
+
+def buildTree(numsBFS: List[int]) -> Optional[TreeNode]:
+    queue = collections.deque()
+    result = None
+    for i, val in enumerate(numsBFS):
+        node = None
+        if val != None:
+            node = TreeNode(val)
+            queue.append(node)
+        if not result:
+            result = node
+        else:
+            if i & 1:
+                queue[0].left = node
+            else:
+                queue[0].right = node
+                queue.popleft()
+    return result
+
+
+if __name__ == '__main__':
+    unittest.main()
