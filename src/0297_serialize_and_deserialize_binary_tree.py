610ad13463bf941c45e0f199b8c6e52e1ddb0847
Kuba Buda <kubabuda@gazeta.pl>
2022-10-22 23:38:06 +0200

    297. Serialize and Deserialize Binary Tree: Recursive DFS with preorder traversal

--- src/0297_serialize_and_deserialize_binary_tree.py
+++ src/0297_serialize_and_deserialize_binary_tree.py
@@ -11,7 +11,7 @@ class TreeNode:
         return f"TreeNode {self.val}{f' l:({self.left})' if self.left else ''}{f' r:({self.right})' if self.right else ''}"
 
 class Codec:
-    
+    """BFS with queue"""
     def serialize(self, root):
         values = []
         q = collections.deque([root])
@@ -49,6 +49,38 @@ class Codec:
         return root
 
 
+class Codec2:
+    """Recursive DFS with preorder traversal"""
+    def serialize(self, root):
+        values = []
+        def dfs(root):
+            if root:
+                values.append(str(root.val))
+                dfs(root.left)
+                dfs(root.right)
+            else:
+                values.append('N')
+        
+        dfs(root)
+        return ";".join(values)
+        
+
+    def deserialize(self, data):
+        values = [ int(v) if v != "N" else None for v in data.split(';')]
+
+        def dfs(i):
+            val = values[i]
+            i += 1
+            node = None
+            if val != None:
+                node = TreeNode(val)
+                node.left, i = dfs(i)
+                node.right, i = dfs(i)
+            return node, i
+
+        root, _ = dfs(0)
+        return root
+
 from unittest import TestCase
 import unittest
 import collections
@@ -63,7 +95,7 @@ class CodecTests(unittest.TestCase):
         ([4,-7,-3,null,null,-9,-3,9,-7,-4,null,6,null,-6,-6,null,null,0,6,5,null,9,null,null,-1,-4,null,null,null,-2]),
     ]
 
-    def testCases_levelOrder(self):
+    def testCases_codec(self):
         def dfs_assertEqual(n1, n2):
             if (n1 and not n2) or (not n1 and n2):
                 self.assertEqual(n1, n2, (n1, n2))
@@ -79,6 +111,26 @@ class CodecTests(unittest.TestCase):
                 # act
                 ser = Codec()
                 deser = Codec()
+                ans = deser.deserialize(ser.serialize(root))
+                # assert
+                dfs_assertEqual(root, ans)
+    
+    def testCases_codec2(self):
+        def dfs_assertEqual(n1, n2):
+            if (n1 and not n2) or (not n1 and n2):
+                self.assertEqual(n1, n2, (n1, n2))
+            if n1 and n2:
+                self.assertEqual(n1.val, n2.val, (n1, n2))
+                if n1.left: dfs_assertEqual(n1.left, n2.left)
+                if n1.right: dfs_assertEqual(n1.right, n2.right)
+
+        for nums  in self.param_list:
+            with self.subTest():
+                # arrange
+                root = buildTree(nums)
+                # act
+                ser = Codec2()
+                deser = Codec2()
                 ans = deser.deserialize(ser.serialize(root))
                 # assert
                 dfs_assertEqual(root, ans)
