aa07efcd0b26d39c44ff7360cf9a904d5f5dbdfc
Kuba Buda <kubabuda@gazeta.pl>
2022-10-24 08:29:13 +0200

    36. Valid Sudoku

---
+++ src/0036_valid_sudoku.py
@@ -0,0 +1,67 @@
+from typing import *
+# 36. Valid Sudoku
+# https://leetcode.com/problems/valid-sudoku//
+
+class Solution:
+    def isValidSudoku(self, board: List[List[int]]) -> bool:
+        rows = [set() for i in range(9)]
+        cols = [set() for i in range(9)]
+        boxes = [[set() for col in range(3)] for row in range(3)]
+
+        for row in range(9):
+            for col in range(9):
+                val = board[row][col]
+                if val == '.': continue
+                
+                if val in rows[row]: return False
+                rows[row].add(val)
+                
+                if val in cols[col]: return False
+                cols[col].add(val)
+
+                brow, bcol = int(row/3), int(col/3)             
+                if val in boxes[brow][bcol]:
+                    return False
+                boxes[brow][bcol].add(val)
+        return True
+
+from unittest import TestCase
+import unittest
+
+class SolutionTests(unittest.TestCase): 
+    
+    param_list = lambda self: [
+        ([["5","3",".",".","7",".",".",".","."],
+          ["6",".",".","1","9","5",".",".","."],
+          [".","9","8",".",".",".",".","6","."],
+          ["8",".",".",".","6",".",".",".","3"],
+          ["4",".",".","8",".","3",".",".","1"],
+          ["7",".",".",".","2",".",".",".","6"],
+          [".","6",".",".",".",".","2","8","."],
+          [".",".",".","4","1","9",".",".","5"],
+          [".",".",".",".","8",".",".","7","9"]], True            
+        ),
+        ([["8","3",".",".","7",".",".",".","."],
+          ["6",".",".","1","9","5",".",".","."],
+          [".","9","8",".",".",".",".","6","."],
+          ["8",".",".",".","6",".",".",".","3"],
+          ["4",".",".","8",".","3",".",".","1"],
+          ["7",".",".",".","2",".",".",".","6"],
+          [".","6",".",".",".",".","2","8","."],
+          [".",".",".","4","1","9",".",".","5"],
+          [".",".",".",".","8",".",".","7","9"]], False
+        ),
+    ]
+
+    def testCases_isValidSudoku(self):
+        for board, expected in self.param_list():
+            with self.subTest():
+                # arrange
+                sol = Solution()
+                # act
+                result = sol.isValidSudoku(board)
+                # assert
+                self.assertEqual(expected, result, (board))
+
+if __name__ == '__main__':
+    unittest.main()
