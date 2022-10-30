3f650c81513af0afc320495f8b68a05d91030e90
Kuba Buda <kubabuda@gazeta.pl>
2022-10-30 09:48:43 +0100

    37. Sudoku Solver: backtrack

--- src/0037_sudoku_solver.py
+++ src/0037_sudoku_solver.py
@@ -3,27 +3,39 @@ from typing import *
 # https://leetcode.com/problems/sudoku_solver
 
 class Solution:
-    def isValidSudoku(self, board: List[List[int]]) -> bool:
-        rows = [set() for i in range(9)]
-        cols = [set() for i in range(9)]
-        boxes = [[set() for col in range(3)] for row in range(3)]
+    def solveSudoku(self, board: List[List[int]]) -> None:
+        """"Computerphile algo with backtrack"""
+        def isPossible(row, col, val) -> bool:
+            for i in range(9):
+                if board[row][i] == val: return False
+                if board[i][col] == val: return False
+            grrow, grcol = (row // 3) * 3, (col // 3) * 3
+            for i in range(3):
+                for j in range(3):
+                    if board[grrow+i][grcol+j] == val: 
+                        return False
+            return True
+        
+        values = ['1','2','3','4','5','6','7','8','9']
+        empty = '.'
+        solved = False
 
-        for row in range(9):
-            for col in range(9):
-                val = board[row][col]
-                if val == '.': continue
-                
-                if val in rows[row]: return False
-                rows[row].add(val)
-                
-                if val in cols[col]: return False
-                cols[col].add(val)
+        def solve(board):
+            nonlocal solved
+            for row in range(9):
+                for col in range(9):
+                    if board[row][col] == empty:
+                        if solved: break
+                        for value in values:
+                            if not solved and isPossible(row, col, value):
+                                board[row][col] = value
+                                solve(board)
+                                if not solved: 
+                                    board[row][col] = empty # backtrack
+                        return
+            solved = True
 
-                brow, bcol = int(row/3), int(col/3)             
-                if val in boxes[brow][bcol]:
-                    return False
-                boxes[brow][bcol].add(val)
-        return True
+        solve(board)
 
 
 import unittest
@@ -39,29 +51,28 @@ class SolutionTests(unittest.TestCase):
           ["7",".",".",".","2",".",".",".","6"],
           [".","6",".",".",".",".","2","8","."],
           [".",".",".","4","1","9",".",".","5"],
-          [".",".",".",".","8",".",".","7","9"]], True            
-        ),
-        ([["8","3",".",".","7",".",".",".","."],
-          ["6",".",".","1","9","5",".",".","."],
-          [".","9","8",".",".",".",".","6","."],
-          ["8",".",".",".","6",".",".",".","3"],
-          ["4",".",".","8",".","3",".",".","1"],
-          ["7",".",".",".","2",".",".",".","6"],
-          [".","6",".",".",".",".","2","8","."],
-          [".",".",".","4","1","9",".",".","5"],
-          [".",".",".",".","8",".",".","7","9"]], False
+          [".",".",".",".","8",".",".","7","9"]],
+         [["5","3","4","6","7","8","9","1","2"],
+          ["6","7","2","1","9","5","3","4","8"],
+          ["1","9","8","3","4","2","5","6","7"],
+          ["8","5","9","7","6","1","4","2","3"],
+          ["4","2","6","8","5","3","7","9","1"],
+          ["7","1","3","9","2","4","8","5","6"],
+          ["9","6","1","5","3","7","2","8","4"],
+          ["2","8","7","4","1","9","6","3","5"],
+          ["3","4","5","2","8","6","1","7","9"]]      
         ),
     ]
 
-    def testCases_isValidSudoku(self):
+    def testCases_solveSudoku(self):
         for board, expected in self.param_list():
             with self.subTest():
                 # arrange
                 sol = Solution()
                 # act
-                result = sol.isValidSudoku(board)
+                sol.solveSudoku(board)
                 # assert
-                self.assertEqual(expected, result, (board))
+                self.assertEqual(expected, board)
 
 if __name__ == '__main__':
     unittest.main()
