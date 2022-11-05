66757b8648410671fa2827f2b6b39145fb7dc7c8
Kuba Buda <kubabuda@gazeta.pl>
2022-11-05 12:07:52 +0100

    212. Word Search II: start work on pruning to prevent TLE

--- src/0212_word-search-ii.py
+++ src/0212_word-search-ii.py
@@ -5,37 +5,7 @@ import collections
 # https://leetcode.com/problems/design-add-and-search-words-data-structure/
 
 class Solution:
-    def findWords1(self, board: List[List[str]], words: List[str]) -> List[str]:
-        letters = {}
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                c = board[i][j]
-                if c not in letters: letters[c] = set()
-                letters[c].add((i,j))
-
-        def isinboard(word, path=None, prev_xy=None) -> bool:
-            if not word: return True
-            if word[0] not in letters: return False
-
-            if not path:
-                for xy in letters[word[0]]:
-                    if isinboard(word[1:], set([xy]), xy): 
-                        return True
-            else:
-                x, y = prev_xy
-                curr = [(x - 1, y), (x + 1, y),(x, y - 1), (x, y + 1)]
-                for xy in curr:
-                    if xy in letters[word[0]] and xy not in path:
-                        path.add(xy)
-                        if isinboard(word[1:], path, xy):
-                            return True
-                        path.remove(xy)
-            return False
-
-        return [w for w in words if isinboard(w)] 
-
-
-    def findWords2(self, board: List[List[str]], words: List[str]) -> List[str]:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         root = {}
         EOL = '\0'
         
@@ -80,6 +50,108 @@ class Solution:
         return [w for w in result]
 
 
+    def findWords1(self, board: List[List[str]], words: List[str]) -> List[str]:
+        letters = {}
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                c = board[i][j]
+                if c not in letters: letters[c] = set()
+                letters[c].add((i,j))
+
+        def isinboard(word, path=None, prev_xy=None) -> bool:
+            if not word: return True
+            if word[0] not in letters: return False
+
+            if not path:
+                for xy in letters[word[0]]:
+                    if isinboard(word[1:], set([xy]), xy): 
+                        return True
+            else:
+                x, y = prev_xy
+                curr = [(x - 1, y), (x + 1, y),(x, y - 1), (x, y + 1)]
+                for xy in curr:
+                    if xy in letters[word[0]] and xy not in path:
+                        path.add(xy)
+                        if isinboard(word[1:], path, xy):
+                            return True
+                        path.remove(xy)
+            return False
+
+        return [w for w in words if isinboard(w)] 
+
+
+    def findWords2(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = {}
+        EOL = '\0'
+        
+        def addWord(word) -> None:
+            node = root
+            for c in word:
+                if not c in node: node[c] = {}
+                node = node[c]
+            node[EOL] = None
+
+        def pruneWord(word) -> None:
+            node = root
+            toPrune=[]
+            # TODO prune word from 
+
+        for word in words: addWord(word)
+        result, visited = set(), set()
+        ROWS, COLS = len(board), len(board[0])
+
+        def dfs(x, y, node, word=''):
+            if x < 0 or x >= ROWS or y < 0 or y >= COLS\
+            or (x,y) in visited or not board[x][y] in node: return
+            visited.add((x,y))
+
+            c = board[x][y]
+            node = node[c]
+            word = f'{word}{c}'
+
+            if EOL in node:
+                result.add(word)
+                node.pop(EOL)
+                pruneWord(word)
+
+            dfs(x-1, y, node, word)
+            dfs(x+1, y, node, word)
+            dfs(x, y-1, node, word)
+            dfs(x, y+1, node, word)
+    
+            visited.remove((x,y))
+
+        for x in range(ROWS):
+            for y in range(COLS):
+                dfs(x,y, root)
+        
+        return list(result)
+
+
+###############################################################################
+ def addWord(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isWord = True
+        
+    def pruneWord(self, word) -> None:
+        cur: TrieNode = self
+        nodeAndChildKey: list[tuple[TrieNode, str]] = []
+        for char in word:
+            nodeAndChildKey.append((cur, char))
+            cur = cur.children[char]
+
+        for parentNode, childKey in reversed(nodeAndChildKey):
+            targetNode = parentNode.children[childKey]
+            if len(targetNode.children) == 0:
+                del parentNode.children[childKey]
+            else:
+                return
+######################################################################
+
 import unittest
 import collections
 
@@ -102,6 +174,16 @@ class SolutionTests(unittest.TestCase):
             ["eat","oath"]
         ),
     ]
+
+    def testCases_findWords(self):
+        for board, words, expected in self.param_list:
+            with self.subTest():
+                # arrange
+                sut = Solution()
+                # act
+                result = sut.findWords(board, words)
+                # assert
+                self.assertEqual(sorted(expected), sorted(result), (board, words))
 
     def testCases_findWords1(self):
         for board, words, expected in self.param_list:
