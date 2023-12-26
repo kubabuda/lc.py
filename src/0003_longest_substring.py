cf9e4ed0b4e12446cf92b0fbcdf3178de237822c
Kuba Buda <kubabuda@gazeta.pl>
2023-12-26 10:52:42 +0100

    3. Longest Substring Without Repeating Characters - two pointer approach

--- src/0003_longest_substring.py
+++ src/0003_longest_substring.py
@@ -15,7 +15,36 @@ class Solution:
             maxL = max(i + 1 - start, maxL)
         return maxL
 
+    def lengthOfLongestSubstring_brute(self, s: str) -> int:
+        i = 0
+        maxL = 0
+        while i < len(s):
+            used = {}
+            j = i
+            while j < len(s) and s[j] not in used:
+                used[s[j]] = True
+                j += 1
+                maxL = max(maxL, j - i)
+            i += 1
+        return maxL
 
+    def lengthOfLongestSubstring_twoPointers(self, s: str) -> int:
+        maxL = 0
+        l = 0
+        r = 0
+        used = {}
+
+        while r < len(s):
+            while r < len(s) and s[r] not in used:
+                used[s[r]] = r
+                r += 1
+                maxL = max(maxL, r - l)
+            while r < len(s) and s[r] in used:
+                del used[s[l]]
+                l += 1
+
+        return maxL
+        
 
 import unittest
 
@@ -31,6 +60,7 @@ class SolutionTests(unittest.TestCase):
         ('pwwkew', 3),
         ('abba', 2),
         ('abb', 2),
+        ('dvdf', 3)
     ]
 
     def testCases_lengthOfLongestSubstring(self):
@@ -40,6 +70,28 @@ class SolutionTests(unittest.TestCase):
                 s = Solution()
                 # act
                 result = s.lengthOfLongestSubstring(word)
+                # assert
+                self.assertEqual(expected, result, (word))
+
+
+    def testCases_lengthOfLongestSubstring_brute(self):
+        for word, expected in self.param_list():
+            with self.subTest():
+                # arrange
+                s = Solution()
+                # act
+                result = s.lengthOfLongestSubstring_brute(word)
+                # assert
+                self.assertEqual(expected, result, (word))
+    
+
+    def testCases_lengthOfLongestSubstring_twoPointers(self):
+        for word, expected in self.param_list():
+            with self.subTest():
+                # arrange
+                s = Solution()
+                # act
+                result = s.lengthOfLongestSubstring_twoPointers(word)
                 # assert
                 self.assertEqual(expected, result, (word))
 
