fbf58f3be18aa8dd0fce1508b333f46cad55b274
Kuba Buda <kubabuda@gazeta.pl>
2024-01-02 19:38:04 +0100

    2610. Convert an Array Into a 2D Array With Conditions: frequency map

--- src/2610_convert-an-array-into-a-2d-array-with-conditions.py
+++ src/2610_convert-an-array-into-a-2d-array-with-conditions.py
@@ -9,21 +9,35 @@ class Solution:
         for n in nums:
             ts = None
             for s in sets:
-                if n not in s:
+                if not n in s:
                     ts = s
-            if not ts:
+                    break
+            if ts is None:
                 ts = set()
                 sets.append(ts)
             ts.add(n)
         return [list(s) for s in sets]        
+
+    '''frequency map: N space N time'''
+    def findMatrix_freqMap(self, nums: List[int]) -> List[List[int]]:
+        freq = {}
+        for n in nums:
+            if n not in freq:
+                freq[n] = 0
+            freq[n] += 1
+        cnt = max(freq[n] for n in freq)
+        result = [[] for i in range(cnt)]
+        for n in freq:
+            for i in range(freq[n]):
+                result[i].append(n)
+        return result
 
 
 import unittest
 null = None
 class SolutionTests(unittest.TestCase): 
     param_list = lambda _:[
-        # ([1,3,4,1,2,3,1], [[1,3,4,2],[1,3],[1]]), # why?
-        ([1,3,4,1,2,3,1], [[1,3,2],[1,3,4],[1]]),
+        ([1,3,4,1,2,3,1], [[1,3,4,2],[1,3],[1]]),
         ([1,2,3,4], [[4,3,2,1]]),
     ]
 
@@ -39,6 +53,20 @@ class SolutionTests(unittest.TestCase):
                     sorted((sorted(i) for i in expected)),
                     sorted((sorted(i) for i in result)),
                     (nums))
+
+    def testCases_findMatrix_freqMap(self):
+        for nums, expected in self.param_list():
+            with self.subTest():
+                # arrange
+                sol = Solution()
+                # act
+                result = sol.findMatrix_freqMap(nums)
+                # assert
+                self.assertEqual(
+                    sorted((sorted(i) for i in expected)),
+                    sorted((sorted(i) for i in result)),
+                    (nums))
+
 
 if __name__ == '__main__':
     unittest.main()
