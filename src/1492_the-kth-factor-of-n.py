2ac4dc97f65881261bb31ed9de9cbb9d527c3452
Kuba Buda <kubabuda@gazeta.pl>
2024-01-02 19:40:21 +0100

    1492. The kth Factor of n. O(sqrt N) space O(sqrt N) time

--- src/1492_the-kth-factor-of-n.py
+++ src/1492_the-kth-factor-of-n.py
@@ -15,6 +15,26 @@ class Solution:
         if len(factors) < k:
             return -1
         return factors[k - 1]
+    
+    '''O(sqrt N) space O(sqrt N) time'''
+    def kthFactorS(self, n: int, k: int) -> int:
+        lt = []
+        gt = []
+        csqrt = ceil(sqrt(n))
+        for i in range (1, csqrt + 1):
+            d = n // i 
+            if n // i == n / i:
+                lt.append(i)
+                if d > csqrt:
+                    gt.append(d)
+        if len(lt) == 0: 
+            lt.append(1)
+        if len(lt) + len(gt) < k:
+            return -1
+        if len(lt) >= k:
+            return lt[k - 1]
+        return gt[len(lt) + len(gt) - k ]
+
 
 import unittest
 null = None
@@ -42,6 +62,16 @@ class SolutionTests(unittest.TestCase):
                 sol = Solution()
                 # act
                 result = sol.kthFactor(n, k)
+                # assert
+                self.assertEqual(expected, result, (n, k))
+
+    def testCases_kthFactorS(self):
+        for n, k, expected in self.param_list():
+            with self.subTest():
+                # arrange
+                sol = Solution()
+                # act
+                result = sol.kthFactorS(n, k)
                 # assert
                 self.assertEqual(expected, result, (n, k))
 
